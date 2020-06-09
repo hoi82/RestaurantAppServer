695df3d1681a53dbfa6235311ff55e6678cff51c
Yong ho Kim <hoi0622@gmail.com>
2020-06-09 20:13:11 +0900

    06/09 Review Route Refactoring

--- routes/review.js
+++ routes/review.js
@@ -16,8 +16,10 @@ module.exports = (app = require("express")()) => {
 
     //Get Reviews by restaurant ID (Include Reviewer Name)
     app.get("/api/restaurant/:id/reviews", (req, res) => {        
-        Reviews.find({ resID: req.params.id, deleted: false }).skip(Number(req.query.page) * Number(req.query.len))
-        .limit(Number(req.query.len)).then((reviews) => {            
+        Reviews.find({ resID: req.params.id, deleted: false }, [], {
+            skip: Number(req.query.page) * Number(req.query.len),
+            limit: Number(req.query.len),            
+        }, (err, reviews) => {
             const promises = reviews.map((r, i) => (r.toObject())).map((r, i) => {
                 return new Promise((resolve, reject) => {
                     Users.findById(r.userID, (err, user) => {
@@ -39,16 +41,16 @@ module.exports = (app = require("express")()) => {
                 getReviewRatings(req.params.id).then((rating) => {
                     res.json({
                         totalReviews: rating.len,
-                        reviewRating: rating.sum == 0 && rating.len == 0 ? "?" : rating.sum / rating.len,
+                        reviewRating: rating.sum == 0 && rating.len == 0 ? "?" : (rating.sum / rating.len).toFixed(1),
                         reviews: results
                     });
                 });                
             });
-        });
+        })   
     });
 
     //Add Review
-    app.post("/api/review", (req, res) => {
+    app.post("/api/review", (req, res) => {        
         const review = new Reviews();
         review.resID = req.body.resId;
         review.userID = req.session.user.id;
@@ -60,18 +62,20 @@ module.exports = (app = require("express")()) => {
         review.deleted = false;
         review.save({validateBeforeSave: true}, (err, rv) => {
             if (err) {
-                res.json(err);
+                res.json(err);                
             }
             else {
+                
                 res.json(rv);
             }
         })        
     });
 
     //Edit Review
-    app.patch("/api/review", (req, res) => {
+    app.put("/api/review/:id", (req, res) => {
+        console.log("edited");
         const edit = Object.assign({}, req.body, {edited: new Date()});
-        Reviews.findByIdAndUpdate(req.body.id, edit, (err, rev) => {
+        Reviews.findByIdAndUpdate(req.params.id, edit, (err, rev) => {
             if (err) {
                 res.status(404).json(err);
             }
